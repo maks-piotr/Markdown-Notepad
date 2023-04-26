012c02bff1db3bdd2db586f92795e1b9ce96bbec
LikelyCloud <71562020+LikelyCloud@users.noreply.github.com>
2023-04-26 17:44:52 +0200

    Initial layout setup

---
+++ app/src/main/java/com/markdown_notepad/NotesListActivity.kt
@@ -0,0 +1,12 @@
+package com.markdown_notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.markdown_notepad.R
+
+class NotesListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_list)
+    }
+}
---
+++ app/src/main/java/com/markdown_notepad/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.markdown_notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.markdown_notepad.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
